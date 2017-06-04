8f8d210ce0145f85afa1021bc9ac071167543fa9
Anderson Daniel <andersondaniel1980@gmail.com>
2017-06-04 14:05:35 -0300

    get e set

--- src/main/java/entity/Usuario.java
+++ src/main/java/entity/Usuario.java
@@ -16,6 +16,39 @@ public Usuario(Integer idUsuario, String nome, String email, String senha) {
 	this.senha = senha;
 }
 
+public Integer getIdUsuario() {
+	return idUsuario;
+}
+
+public void setIdUsuario(Integer idUsuario) {
+	this.idUsuario = idUsuario;
+}
+
+public String getNome() {
+	return nome;
+}
+
+public void setNome(String nome) {
+	this.nome = nome;
+}
+
+public String getEmail() {
+	return email;
+}
+
+public void setEmail(String email) {
+	this.email = email;
+}
+
+public String getSenha() {
+	return senha;
+}
+
+public void setSenha(String senha) {
+	this.senha = senha;
+}
+
+
 	
   
 } 
